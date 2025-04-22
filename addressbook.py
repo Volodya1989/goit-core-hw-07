0fac73699b7cb2f34a02a662c8973659a4faf78a
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2025-04-22 14:54:50 -0400

    addressBook modified

--- addressbook.py
+++ addressbook.py
@@ -1,4 +1,5 @@
 from collections import UserDict
+from datetime import datetime, date, timedelta
 
 
 class AddressBook(UserDict):
@@ -10,6 +11,22 @@ class AddressBook(UserDict):
 
     def delete(self, name):
         self.data.pop(name, None)
+
+    def get_upcoming_birthdays(self, days=7):
+        upcoming = []
+        today = date.today()
+        for record in self.data.values():
+            if record.birthday:
+                birthday = record.birthday.value.replace(year=today.year)
+                if birthday < today:
+                    birthday = birthday.replace(year=today.year + 1)
+                if birthday.weekday() in (5, 6):  # Saturday, Sunday
+                    birthday += timedelta(days=7 - birthday.weekday())
+                days_diff = (birthday - today).days
+                if 0 <= days_diff <= days:
+                    upcoming.append(
+                        {"name": record.name.value, "birthday": birthday.strftime("%d.%m.%Y")})
+        return upcoming
 
     def __str__(self):
         return "\n".join(map(str, self.data.values()))
